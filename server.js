146cd97b784c1699e96c9e74646145f87af5670d
Clarice-COD <clariceboerlijst@gmail.com>
2025-04-14 14:50:15 +0200

    Server basis opzet

--- server.js
+++ server.js
@@ -3,3 +3,57 @@ console.log('Hier komt je server voor Sprint 10.')
 console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
 
 console.log('Zet \'m op!')
+
+// Basis instellingen
+import express, {json, response, text} from 'express'
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+
+app.set('views', './views')
+
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+})
+
+// web pagina's
+
+app.get('/', (req, res) => {
+    res.render('index.liquid')
+})
+
+app.get('/', (req, res) => {
+    res.render('about.liquid')
+})
+
+app.get('/', (req, res) => {
+    // connectie maken directus
+    // query uitvoeren voor het ophalen van de laatste 10 blogs gesorteerd
+    // data controleren en filteren op volledigheid
+    // bewerking uitvoeren op de data
+    // data doorgeven aan de template
+    res.render('blog.liquid')
+})
+
+app.get('/', (req, res) => {
+    // connectie maken directus
+    // voer de post actie uit
+    // query uitvoeren voor het ophalen van de laatste 5 projecten gesorteerd
+    // data controleren en filteren op volledigheid
+    // bewerking uitvoeren op de data
+    // data doorgeven aan de template
+    res.redirect('/blog')
+})
+
+app.get('/', (req, res) => {
+    res.render('contact.liquid')
+})
