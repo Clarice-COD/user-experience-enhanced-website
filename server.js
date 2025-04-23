eb89e0d235aa1970d2674a10e7ba1c74058e41ff
Clarice-COD <clariceboerlijst@gmail.com>
2025-04-23 12:59:41 +0200

    Probleem render error opgelost
    Ik had index.liquid onder de map views inplaats van erin staan.

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-console.log('Hier komt je server voor Sprint 10.')
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
 
-console.log('Zet \'m op!')
 
 // Basis instellingen
 import express, {json, response, text} from 'express'
 import { Liquid } from 'liquidjs';
 
 const app = express()
-
 
 app.use(express.urlencoded({extended: true}))
 
@@ -19,11 +15,6 @@ const engine = new Liquid();
 app.engine('liquid', engine.express());
 
 app.set('views', './views')
-
-app.set('port', process.env.PORT || 8000)
-app.listen(app.get('port'), function () {
-  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
-})
 
 // web pagina's
 
@@ -56,4 +47,9 @@ app.get('/', (req, res) => {
 
 app.get('/', (req, res) => {
     res.render('contact.liquid')
+})
+
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
 })
